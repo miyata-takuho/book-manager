78cae47347f980eba4d5a8c21f76d5813e630bde
miyata-takuho <57648765+miyata-takuho@users.noreply.github.com>
2019-11-28 09:59:19 +0900

    edit

--- book_manager/app/controllers/books_controller.rb
+++ book_manager/app/controllers/books_controller.rb
@@ -1,5 +1,23 @@
 class BooksController < ApplicationController
   def show
-     @book = Book.find(params[:id])
+   end
+   def index
+     @books = Book.where(params[:id])
+     @books.each do |book|
+        book.title
+      end
+   end
+
+   def create
+     @books = Book.create
+      if @books.save
+       render("books/index")
+      else
+       render("main/home")
+      end
+    end
+
+   def new
+     @books = Book.new
    end
  end
--- book_manager/app/controllers/main_controller.rb
+++ book_manager/app/controllers/main_controller.rb
@@ -1,6 +1,16 @@
 class MainController < ApplicationController
   def home
+     @books = Book.new
   end
+
+  # def create
+  #   @books = Book.save
+  #   if @books.save
+  #     redirect_to("/books/index")
+  #   else
+  #     render("main/home")
+  # end
+  # end
 
   def user
   end
