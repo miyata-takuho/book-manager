c6493868e9a75842789c1891cfafb8876d928164
miyata-takuho <57648765+miyata-takuho@users.noreply.github.com>
2019-12-11 09:30:28 +0900

    Revert "added devise gem wip"
    This reverts commit df60a3691485655a002b1a3fa35495b5ae3e61f4.

--- book_manager/config/environment.rb
+++ book_manager/config/environment.rb
@@ -3,7 +3,3 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-Rails.application.configure do
-config.action_mailer.default_url_options = { host: 'localhost', port: 3003 }
-end
