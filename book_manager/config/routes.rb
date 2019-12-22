d02c17a6b268bf1e1c5f58c97a885227231e9832
miyata-takuho <57648765+miyata-takuho@users.noreply.github.com>
2019-12-22 19:25:51 +0900

    devise追加
    サインイン、ログイン、ログアウトでのエラーはないが、サインインしていなくても書籍一覧ページにいける状態

--- book_manager/config/routes.rb
+++ book_manager/config/routes.rb
@@ -1,5 +1,23 @@
 Rails.application.routes.draw do
-  root 'books#new'
+  devise_for :users, :controllers => {
+    :registrations => 'users/registrations',
+    :sessions => 'users/sessions'
+  }
+
+  # resources :users
+
+  devise_scope :user do
+    root 'users/registrations#new'
+    get "user/:id", :to => "users/registrations#detail"
+    get "signup", :to => "users/registrations#new"
+    get "login", :to => "users/sessions#new"
+    get "logout", :to => "users/sessions#destroy"
+    get "/users/sign_out", :to => "users/sessions#destroy"
+  end
+
+
+  # root 'books#new'
+
 
   get 'user/user'
   get "books/index" => "books#index"
@@ -10,6 +28,13 @@ Rails.application.routes.draw do
   post "books/:id/update" => "books#update"
   post "books/:id/destroy" => "books#destroy"
 # get 'book/new/:id', to: 'books#new', as: :books_new
+
+
+
+
+
+
+
 
   resources :books
 
