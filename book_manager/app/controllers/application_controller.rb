855dc155b99415db87455c6885e4119acdba59b3
miyata-takuho <57648765+miyata-takuho@users.noreply.github.com>
2020-01-22 08:06:09 +0900

    fix

--- book_manager/app/controllers/application_controller.rb
+++ book_manager/app/controllers/application_controller.rb
@@ -8,13 +8,6 @@ class ApplicationController < ActionController::Base
 
     protected
 
-  # def configure_permitted_parameters
-  #   added_attrs = [:name, :username, :email, :password, :password_confirmation, :remember_me]
-  #   devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
-  #   devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
-  #   devise_parameter_sanitizer.permit :account_update, keys: added_attrs
-  # end
-
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
